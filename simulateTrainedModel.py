022959633dd86d27c5c7e2ff63247b5aaa001ba8
Santosh Manicka <santosh.manicka@tufts.edu>
2024-05-24 11:30:44 +0530

    New shell script and missed commits

---
+++ simulateTrainedModel.py
@@ -0,0 +1,30 @@
+from cellularFieldNetwork import cellularFieldNetwork
+import numpy as np
+import torch
+
+parameterfilename = './data/bestModelParameters_0.dat'
+parameters = torch.load(parameterfilename)
+
+latticeDims = parameters['latticeDims']
+fieldParameters = parameters['fieldParameters']
+GRNParameters = parameters['GRNParameters']
+numSamples = parameters['simParameters']['numSamples']
+initialValues = parameters['simParameters']['initialValues']
+clampParameters = parameters['clampParameters']
+externalInputs = parameters['simParameters']['externalInputs']
+numSimIters = parameters['simParameters']['numSimIters']
+evalDurationProp = parameters['trainParameters']['evalDurationProp']
+targetVmem = parameters['trainParameters']['targetVmem']
+
+modelparameters = dict()
+modelparameters['fieldParameters'] = fieldParameters
+modelparameters['GRNParameters'] = GRNParameters
+circuit = cellularFieldNetwork(latticeDims=latticeDims,parameters=modelparameters,numSamples=numSamples)
+circuit.initVariables(initialValues)
+circuit.initParameters(initialValues)
+circuit.simulate(externalInputs=externalInputs,clampParameters=clampParameters,perturbationParameters=None,
+				 numSimIters=numSimIters,stochasticIonChannels=False,setGradient=False,retainGradients=False,saveData=True)
+evalDuration = int(evalDurationProp*numSimIters)
+loss = ((targetVmem - circuit.timeseriesVmem[-evalDuration:]) ** 2).sum().sqrt()
+np.set_printoptions(precision=2,suppress=True)
+print("loss = ",loss)
