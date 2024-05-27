783dac571ad7c4f8fa29e89d7670b1b340dc38e1
Santosh Manicka <santosh.manicka@tufts.edu>
2024-05-27 10:38:34 +0530

    Modified script to include on-the-fly parameters

--- runLearnCellularFieldNetwork.sh
+++ runLearnCellularFieldNetwork.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 latticeDims="(11,11)"
 fieldResolution=1
-fieldAggregation="sum"
+#fieldAggregation="sum"
 fieldScreenSize=4
 #clampMode="fieldDome"
 #clampType="oscillatory"
@@ -19,4 +19,4 @@ python learnCellularFieldNetwork.py --latticeDims $latticeDims --fieldResolution
 #python learnCellularFieldNetwork.py --latticeDims $latticeDims --fieldResolution $fieldResolution --fieldAggregation $fieldAggregation --fieldScreenSize $fieldScreenSize --clampMode $clampMode --clampType $clampType --clampedCellsProp $clampedCellsProp --clampDurationProp $clampDurationProp --clampAmplitudeRange $clampAmplitudeRange --clampFrequencyRange $clampFrequencyRange --numClampCoreSquares $numClampCoreSquares --numSamples $numSamples --numSimIters $numSimIters --numLearnIters $numLearnIters --learnedParameters $learnedParameters --fileNumber $SLURM_ARRAY_TASK_ID --verbose $verbose
 #sbatch --export=ALL --time 2-00:00:00 -p batch --array 1-100 -e Error_%A_%a.err --mem 12G runLearnCellularFieldNetwork.sh
 #sbatch --export=ALL --time 2-00:00:00 -p batch --array 101-200 -e Error_%A_%a.err --mem 4G runLearnCellularFieldNetwork.sh
-#sbatch --export=ALL,clampMode=fieldDomeTwoFoldSymmetry,clampType=oscillatory --time 2-00:00:00 -p batch --array 201-300 -e Error_%A_%a.err --mem 4G runLearnCellularFieldNetwork.sh
+#sbatch --export=ALL,fieldAggregation=sum,clampMode=fieldDomeTwoFoldSymmetry,clampType=oscillatory --time 2-00:00:00 -p batch --array 201-300 -e Error_%A_%a.err --mem 4G runLearnCellularFieldNetwork.sh
