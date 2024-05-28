ebcd11feea9562a8550cc77783beed5267048392
Santosh Manicka <santosh.manicka@tufts.edu>
2024-05-28 18:31:43 +0530

    Some minor fixes

--- runLearnCellularFieldNetwork.sh
+++ runLearnCellularFieldNetwork.sh
@@ -12,8 +12,8 @@ clampAmplitudeRange="(-1.0,1.0)"
 clampFrequencyRange="(100.0,1000.0)"
 numClampCoreSquares=1
 numSamples=1
-numSimIters=100
-numLearnIters=10000
+#numSimIters=100
+#numLearnIters=10000
 #SLURM_ARRAY_TASK_ID=0
 #verbose="True"
 learnedParameters="['clampFrequencies','clampPhases','fieldTransductionWeight','fieldTransductionBias']"
@@ -22,4 +22,4 @@ python learnCellularFieldNetwork.py --latticeDims $latticeDims --fieldResolution
 #sbatch --export=ALL --time 2-00:00:00 -p batch --array 1-100 -e Error_%A_%a.err --mem 12G runLearnCellularFieldNetwork.sh
 #sbatch --export=ALL --time 2-00:00:00 -p batch --array 101-200 -e Error_%A_%a.err --mem 4G runLearnCellularFieldNetwork.sh
 #sbatch --export=ALL,fieldResolution=4,fieldAggregation=sum,clampMode=fieldDome,clampType=oscillatory,verbose=True --time 2-00:00:00 -p batch --array 401-500 -e Error_%A_%a.err --mem 4G runLearnCellularFieldNetwork.sh
-#sbatch --export=ALL,fieldResolution=1,fieldAggregation=average,fieldScreenSize=4,GJStrength=0.25,clampMode=fieldDomeTwoFoldSymmetry,clampType=oscillatory,verbose=False --time 2-00:00:00 -p batch --array 501-600 -e Error_%A_%a.err --mem 4G runLearnCellularFieldNetwork.sh
+#sbatch --export=ALL,fieldResolution=1,fieldAggregation=average,fieldScreenSize=4,GJStrength=0.05,clampMode=fieldDomeTwoFoldSymmetry,clampType=oscillatory,numSimIters=1000,numLearnIters=100000,verbose=False --time 2-00:00:00 -p batch --array 701-800 -e Error_%A_%a.err --mem 2G runLearnCellularFieldNetwork.sh
