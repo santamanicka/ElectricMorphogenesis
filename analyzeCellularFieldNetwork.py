8e6cdd7cfcb546583a9526e9f2806a4c3e16d832
Santosh Manicka <santosh.manicka@tufts.edu>
2024-08-08 18:01:39 +0530

    Minor bug fix

--- analyzeCellularFieldNetwork.py
+++ analyzeCellularFieldNetwork.py
@@ -264,7 +264,7 @@ if analysisMode == 'fixScreenGJSweepWeightBias':  # total parameter combinations
     parameterGrid = list(zip(np.repeat(fieldTransductionWeights,len(fieldTransductionBiases)),
                              np.tile(fieldTransductionBiases,len(fieldTransductionWeights))))
     fieldTransductionTimeConstant = torch.DoubleTensor([10.0])
-    parameterCombination = parameterGrid[fileNumber - 1]  # so file numbers can start from 1
+    parameterCombination = parameterGrid[int(fileNumber) - 1]  # so file numbers can start from 1
     clampParameters = None
 elif analysisMode == 'fixWeightBiasSweepScreenGJ':  # total parameter combinations = 15x20 = 300
     maxFieldScreenSize = 2*max(circuitDims)-1  # the field will permeate the entire tissue = 2(l-1)+1, where l is the max of circuitDims
@@ -273,7 +273,7 @@ elif analysisMode == 'fixWeightBiasSweepScreenGJ':  # total parameter combinatio
     parameterGrid = list(zip(np.repeat(fieldScreenSizes,len(GJStrengths)),
                              np.tile(GJStrengths,len(fieldScreenSizes))))
     fieldTransductionTimeConstant = torch.DoubleTensor([10.0])
-    parameterCombination = parameterGrid[fileNumber - 1]  # so file numbers can start from 1
+    parameterCombination = parameterGrid[int(fileNumber) - 1]  # so file numbers can start from 1
     clampParameters = None
     # Robustness parameters
     if perturbationMode != 'None':
@@ -296,7 +296,7 @@ elif analysisMode == 'fixBiasSweepWeightScreenGJ':  # total parameter combinatio
     fieldTransductionTimeConstant = torch.DoubleTensor([10.0])
     if fileNumber >= 1:  # choose from parameter grid
         parameterGrid = [(screensize,gj,weight) for screensize in fieldScreenSizes for gj in GJStrengths for weight in fieldTransductionWeights]
-        parameterCombination = parameterGrid[fileNumber - 1]  # so file numbers can start from 1
+        parameterCombination = parameterGrid[int(fileNumber) - 1]  # so file numbers can start from 1
     else:  # choose from passed arguments
         parameterCombination = fieldScreenSize, GJStrength, fieldTransductionWeight
     clampParameters = None
