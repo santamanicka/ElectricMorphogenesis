cb6f49674e8cca84c1d6217e98c79a51237e3cbc
Santosh Manicka <santosh.manicka@tufts.edu>
2024-08-18 14:09:04 +0530

    Modified plot slurm script

--- runPlotAnalysisData.sh
+++ runPlotAnalysisData.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python plotAnalysisData.py --analysisMode $1
-#sbatch --time 0-02:00:00 -p batch --mem 2G runPlotAnalysisData.sh "patternability"
+python plotAnalysisData.py --analysisMode $analysisMode --characteristicNames $1
+#sbatch --export-ALL,analysisMode=fixBiasSweepWeightScreenGJ --time 0-01:00:00 -p batch --mem 2G runPlotAnalysisData.sh "['Covariance']"
