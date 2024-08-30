cdef913ce42e1e4e56b389cffd12b69800d097dc
Santosh Manicka <santosh.manicka@tufts.edu>
2024-08-30 13:48:14 +0530

    Modified script

--- runPlotAnalysisData.sh
+++ runPlotAnalysisData.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python plotAnalysisData.py --analysisMode $analysisMode --characteristicNames $1
-#sbatch --export-ALL,analysisMode=fixBiasSweepWeightScreenGJ --time 0-01:00:00 -p batch --mem 2G runPlotAnalysisData.sh "['Covariance']"
+python plotAnalysisData.py --analysisMode $analysisMode --characteristicNames $1 --sample $sample
+#sbatch --export=ALL,analysisMode=fixBiasSweepWeightScreenGJ,sample=Homogenous --time 0-01:00:00 -p batch --mem 2G runPlotAnalysisData.sh "['eVAggVmemDimensionMI']"
