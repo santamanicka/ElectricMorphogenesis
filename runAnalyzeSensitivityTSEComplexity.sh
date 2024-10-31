5fe842f47970e33d3da64f04317c6ca6c52b173b
Santosh Manicka <santosh.manicka@tufts.edu>
2024-10-31 20:12:42 +0530

    Fixed minor bug

--- runAnalyzeSensitivityTSEComplexity.sh
+++ runAnalyzeSensitivityTSEComplexity.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python analyzeSenstivityTSEComplexity.py
-#sbatch --export=ALL --time 1-00:00:00 -p batch --mem 10G runAnalyzeSenstivityTSEComplexity.sh
+python runAnalyzeSensitivityTSEComplexity.py
+#sbatch --export=ALL --time 1-00:00:00 -p batch --mem 10G runAnalyzeSensitivityTSEComplexity.sh
 
