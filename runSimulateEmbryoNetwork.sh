994c107cca990108ff2f0981de235b7669ab3491
Santosh Manicka <santosh.manicka@tufts.edu>
2025-07-07 14:27:31 +0530

    Slightly improved slurm script

--- runSimulateEmbryoNetwork.sh
+++ runSimulateEmbryoNetwork.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-dims="(10,10)"
+dims=$1
 python simulateEmbryoNetwork.py --dims $dims --teratogenExposure $teratogenExposure --modelNumEmbryo $modelNumEmbryo --modelNumATP $modelNumATP --save $save
-#sbatch --export=ALL,teratogenExposure=True,modelNumEmbryo=253,modelNumATP=262,save=True --time 2-00:00:00 -p batch --mem 4G runSimulateEmbryoNetwork.sh
+#sbatch --export=ALL,teratogenExposure=True,modelNumEmbryo=253,modelNumATP=262,save=True --time 2-00:00:00 -p batch --mem 10G runSimulateEmbryoNetwork.sh "(10,10)"
