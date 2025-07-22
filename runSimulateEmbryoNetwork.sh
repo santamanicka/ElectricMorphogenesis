e7c55f3cd8ab6caf0e0a50a4e5e1e64be4f679cb
Santosh Manicka <santosh.manicka@tufts.edu>
2025-07-22 14:01:02 +0530

    Fixed minor bug in script

--- runSimulateEmbryoNetwork.sh
+++ runSimulateEmbryoNetwork.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dims=$1
-python simulateEmbryoNetwork.py --dims $dims --teratogenExposure $teratogenExposure --boundaryAssistance $boundaryAssistance --GRNEnabled $GRNEnabled --LigandEnabled $LigandEnabled --modelNumEmbryo $modelNumEmbryo --modelNumATP $modelNumATP --nsamples $nsamples --niters $niters --sampleNumber $SLURM_ARRAY_TASK_ID --save $save
+python simulateEmbryoNetwork.py --dims $dims --teratogenExposure $teratogenExposure --boundaryAssistance $boundaryAssistance --GRNEnabled $GRNEnabled --LigandEnabled $LigandEnabled --modelNameEmbryo $modelNameEmbryo --modelNameATP $modelNameATP --nsamples $nsamples --niters $niters --sampleNumber $SLURM_ARRAY_TASK_ID --save $save
 #sbatch --export=ALL,teratogenExposure=True,modelNumEmbryo=253,modelNumATP=262,save=True --time 2-00:00:00 -p batch --mem 4G runSimulateEmbryoNetwork.sh "(10,10)"
 #sbatch --export=ALL,teratogenExposure=True,modelNumEmbryo=253,modelNumATP=262,save=True --time 2-00:00:00 -p batch --mem 10G runSimulateEmbryoNetwork.sh "(15,15)"
 #sbatch --export=ALL,teratogenExposure=True,modelNumEmbryo=253,modelNumATP=262,nsamples=1,niters=2000,save=True --time 2-00:00:00 -p batch --array 1-200 -e Error_%A_%a.err --mem 4G runSimulateEmbryoNetwork.sh "(10,10)"
