973e79a7913880cd0b4ec61192beb04d056b3a35
Santosh Manicka <santosh.manicka@tufts.edu>
2025-07-22 14:22:02 +0530

    Updated file naming convention in combineEmbryoNetworkSimFiles.py

--- combineEmbryoNetworkSimFiles.py
+++ combineEmbryoNetworkSimFiles.py
@@ -5,17 +5,22 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dims', type=str, default='(10,10)')
+parser.add_argument('--modelNameEmbryo', type=str, default='False')
+parser.add_argument('--modelNameATP', type=str, default='False')
 parser.add_argument('--samples', type=str, default='(1,100)')
 parser.add_argument('--delete', type=str, default='False')
 
 args = parser.parse_args()
 dims = ast.literal_eval(args.dims)
+modelNameEmbryo = args.modelNameEmbryo
+modelNameATP = args.modelNameATP
 samples = ast.literal_eval(args.samples)
 delete = ast.literal_eval(args.delete)
 
 fulldata = dict()
 for sample in range(samples[0],samples[1]+1):
-    samplefilename = './data/embryoNetSavedSims_' + ','.join(map(str,dims)) + '_sample' + str(sample) + '.dat'
+    Sfx = '_Embryo' + modelNameEmbryo + '_ATP' + modelNameATP + '_'
+    samplefilename = './data/embryoNetSavedSims_' + Sfx + ','.join(map(str,dims)) + '_sample' + str(sample) + '.dat'
     try:
         data = torch.load(samplefilename)
     except:
@@ -25,5 +30,6 @@ for sample in range(samples[0],samples[1]+1):
         if delete:
             os.remove(samplefilename)
 
-fullfilename = './data/embryoNetSavedSims_' + ','.join(map(str,dims)) + '.dat'
+Sfx = '_Embryo' + modelNameEmbryo + '_ATP' + modelNameATP + '_'
+fullfilename = './data/embryoNetSavedSims_' + Sfx + ','.join(map(str,dims)) + '.dat'
 torch.save(fulldata,fullfilename)
